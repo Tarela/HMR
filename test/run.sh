20f5a670e211c8637126a941e8356d1ba5ba6748
Sheng'en Hu <sh8tv@CPHGs-MacBook-Pro.local>
2019-01-11 17:37:44 -0500

    update 20191011, Main function done

---
+++ test/run.sh
@@ -0,0 +1,4 @@
+#export PATH=/Users/sh8tv/Dropbox/CR/HMRpackage/initversion/refpackage/bwsummary:$PATH
+
+./HMR -p /Users/sh8tv/Dropbox/CR/HMR_testdata/mESC_GSM1562337_CBX7.bed -s /Users/sh8tv/Dropbox/CR/HMR_testdata/mESC_GSM1399500_H3K27me3.bw,/Users/sh8tv/Dropbox/CR/HMR_testdata/mESC_GSM1954953_H3K9me3.bw -f /Users/sh8tv/Dropbox/CR/HMR_testdata/TFCRpeak_uniq/ -o test7 --overwrite
+#Rscript tmpPackage/detectNonClassic.r test5 mESC_GSM1399500_H3K27me3,mESC_GSM1954953_H3K9me3 0.001 0.5 1se all /Users/sh8tv/Dropbox/CR/HMRpackage/initversion/test/tmpPackage/
